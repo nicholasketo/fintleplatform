5b94ea5e887f72b3e91b882b055365321c55960c
Nicholas Eto <nicholaseto@Nicholass-MacBook-Pro-2.local>
2023-11-21 21:30:57 -0500

    Commit Nov 23 2023

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React from 'react';
 import './App.css';
+
+function handleClick() {
+  console.log('Button clicked!');
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Fintle: A Financial Literacy Interactive Game for Children!</h1>
       </header>
+      <main>
+        <p>Game made by Nicholas Eto
+        </p>
+        <button onClick = {handleClick}>Play Now</button>
+      </main>
     </div>
   );
 }
